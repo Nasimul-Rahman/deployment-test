1637a741335655f3754c2cd6e5653d467fbf6c94
Nasimul Rahman <nasimul@cynergon.net>
2022-12-02 21:28:46 +0600

    new app base added

--- deploymenttest/settings.py
+++ deploymenttest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^%6n++*)o6wdxcz+dn@9cv4=&)wwx2lq73)0kcm(hky$b6!bh+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ 'web-production-1b1b.up.railway.app']
+ALLOWED_HOSTS = [ '*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base'
 ]
 
 MIDDLEWARE = [
