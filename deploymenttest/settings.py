8402723a0565f4beff5526918877622e236fecdd
Nasimul Rahman <nasimul@cynergon.net>
2022-12-04 03:12:38 +0600

    installed apps changed

--- deploymenttest/settings.py
+++ deploymenttest/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base',
+    'base.apps.BaseConfig',
 
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
