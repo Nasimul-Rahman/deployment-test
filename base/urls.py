436baba5182daf2c1f2b531cb72a4df8cc869fcd
Nasimul Rahman <nasimul@cynergon.net>
2022-12-07 20:41:52 +0600

    about me page added :D

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 #now import the views.py file into this code
-from .views import index
+from .views import index, about
 urlpatterns=[
-  path('', index)
+  path('', index, name='home'),
+  path('about/', about, name='about')
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'base/index.html')
+
+def about(request):
+    return render(request, 'base/about.html')
